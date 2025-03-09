aa12b26c9d085b24d1c674e817ac27e20dbefc2d
BNK4970 <enzocordo66@gmail.com>
2025-03-09 16:56:58 +0100

    docs: correction de majuscule pour le struct

--- backend/main.go
+++ backend/main.go
@@ -13,7 +13,7 @@ import (
 )
 
 // La tructure d'un album
-type album struct {
+type Album struct {
 	ID     string  `json:"id"`
 	Title  string  `json:"title"`
 	Artist string  `json:artist`
@@ -21,7 +21,7 @@ type album struct {
 }
 
 // Notre slice album
-var albums = []album{
+var albums = []Album{
 	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
 	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
 	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
@@ -33,8 +33,11 @@ func getAlbums(c *gin.Context) {
 }
 
 func main() {
+	// Setup du serveur
 	router := gin.Default()
-	router.GET("/albums", getAlbums)
+
+	// Setup des routes api
+	router.GET("/api/albums", getAlbums)
 
 	router.Run("localhost:8080")
 }
