8a077709a5bcb691a4129485ec3eff6455c04a1e
BNK4970 <enzocordo66@gmail.com>
2025-03-09 01:01:03 +0100

    chore: fait avec une doc

---
+++ backend/main.go
@@ -0,0 +1,40 @@
+/*
+ * Fait avec la doc:
+ * https://go.dev/doc/tutorial/web-service-gin
+ */
+
+package main
+
+// Import des blibliothèques
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+// La tructure d'un album
+type album struct {
+	ID     string  `json:"id"`
+	Title  string  `json:"title"`
+	Artist string  `json:artist`
+	Price  float64 `json:price`
+}
+
+// Notre slice album
+var albums = []album{
+	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
+	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
+	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+}
+
+// Fonction pour obtenir notre slice
+func getAlbums(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, albums)
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/albums", getAlbums)
+
+	router.Run("localhost:8080")
+}
